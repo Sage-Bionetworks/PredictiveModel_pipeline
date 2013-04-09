74fa34e0cc6e43137a802c2915390014c5eae13f
Ubuntu <ubuntu@ip-10-113-14-11.ec2.internal>
2013-04-09 05:56:21 +0000

    newest curated datasets in Oct 2012

---
+++ workFlow_ccle_new.R
@@ -0,0 +1,250 @@
+system("mkdir ~/newPredictiveModel_ActArea")
+
+system("mkdir ~/newPredictiveModel_ActArea/E")
+system("mkdir ~/newPredictiveModel_ActArea/C")
+system("mkdir ~/newPredictiveModel_ActArea/Mh")
+system("mkdir ~/newPredictiveModel_ActArea/EMo")
+system("mkdir ~/newPredictiveModel_ActArea/EMh")
+system("mkdir ~/newPredictiveModel_ActArea/CMo")
+system("mkdir ~/newPredictiveModel_ActArea/CMh")
+system("mkdir ~/newPredictiveModel_ActArea/EC")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh")
+
+
+system("mkdir ~/newPredictiveModel_ActArea/EL")
+system("mkdir ~/newPredictiveModel_ActArea/CL")
+system("mkdir ~/newPredictiveModel_ActArea/MhL")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL")
+system("mkdir ~/newPredictiveModel_ActArea/ECL")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL")
+
+
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE")
+
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE")
+
+
+
+
+
+
+
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/ECMh/CCLE/Lasso")
+
+
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/Mh/CCLE/Lasso")
+
+
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/ECMhL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/MhL/CCLE/Lasso")
+
+
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/E/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/C/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/EMo/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/CMo/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/EMh/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/CMh/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/EC/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/ECMo/CCLE/Lasso")
+
+
+
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/EL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/CL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/EMoL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/CMoL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/EMhL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/CMhL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/ECL/CCLE/Lasso")
+
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE/PCR")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE/PLS")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE/SVM")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE/RF")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE/ENet")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE/Ridge")
+system("mkdir ~/newPredictiveModel_ActArea/ECMoL/CCLE/Lasso")
+
+
+workFlow_all<-function(kk){
+  source("~/PredictiveModel_pipeline/myModel.R")
+  source("~/PredictiveModel_pipeline/myData_CCLE_new.R")
+  require(pls)  
+  require(kernlab)
+  require(synapseClient)
+  Drug.type = c("ActArea","EC50","IC50")
+  Data.type = c("Mh","C","CMo","CMh","E","EMo","EMh","EC","ECMo","ECMh","MhL","CL","CMoL","CMhL","EL","EMoL","EMhL","ECL","ECMoL","ECMhL")  
+  Model.type = c("PCR","PLS","Ridge","Lasso","RF","SVM","ENet")
+  for(k0 in Drug.type){
+    for(k1 in Data.type){
+      for(k2 in Model.type){
+        filename = paste("~/PredictiveModel_",k0,"/",k1,"/CCLE/",k2,"/cvDrug_",kk,".Rdata",sep="")
+        if(!file.exists(filename)){
+          resultsScale <- myModel(kk,data.type=k1, drug.type = k0, model.type = k2, nfolds = 5)    
+          save(resultsScale,file = filename)        
+        }
+      }
+    }  
+  }
+}
+
+
+library(multicore)
+library(doMC)
+registerDoMC()
+
+mclapply(1:4, function(x)workFlow_all(x))
+
+
